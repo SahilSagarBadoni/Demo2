793fe5a7e801d1d21f94d8f5bf0206f6b13f0d81
SahilSagarBadoni <85783834+SahilSagarBadoni@users.noreply.github.com>
2021-06-14 16:33:43 +0530

    Add files via upload

---
+++ Java/SahilSagarBadoni-1922054/Demostrate.java
@@ -0,0 +1,8 @@
+class Demostrate
+{
+  public static void main(String[] args)
+   {
+    String s="Sahil";
+    System.out.println(s.toUpperCase());
+   }
+}
---
+++ Java/SahilSagarBadoni-1922054/PrintName.java
@@ -0,0 +1,9 @@
+class PrintName
+{
+  public static void main(String[] args)
+   {
+    int n=1922054;
+    String name="Sahil Sagar Badoni";
+    System.out.println("The name is : "+name+"\n"+"The Roll No. is:"+n);
+   }
+}
